58cff41fb9b264f0b8b932e6ee3546c49af1ed54
levelzero1893 <sami.nobile@outlook.com>
2025-05-18 11:56:49 +0200

    test6

--- flask-app/app-sami.py
+++ flask-app/app-sami.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "SALAM, Loz Townn !"
+	return "SALAM, Loz Townnn !"
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
