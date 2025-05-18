47e7986e7031887304c61acc97d0a32dfc0572fe
levelzero1893 <sami.nobile@outlook.com>
2025-05-18 11:11:20 +0200

    test002

--- flask-app/app-sami.py
+++ flask-app/app-sami.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "SALAM ALYKOUM, Gotham City !"
+	return "SALAM ALYKOUM, City !"
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
